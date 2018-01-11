6458b57b81bf0676b871c117b018f3fea9f96ce6
Jinho Choi <choi@mathcs.emory.edu>
2018-01-11 10:13:52 -0500

    Updated ipython.

--- src/getting_started.py
+++ src/getting_started.py
@@ -13,12 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ========================================================================
-import numpy as np
+import requests
 
 __author__ = 'Jinho D. Choi'
 
-a = np.array([[0, 1, 2], [3, 4, 5]])
-b = np.array([[4, 5, 6], [7, 8, 9]])
-c = a + b
-
-print(c)
+r = requests.get('http://www.mathcs.emory.edu/~choi')
+print(r)
