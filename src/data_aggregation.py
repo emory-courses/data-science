86225f2e6d25553c35867d4168f0fb53412d8f02
Jinho Choi <choi@mathcs.emory.edu>
2018-01-18 15:29:05 -0500

    Updated data aggregation.

--- src/data_aggregation.py
+++ src/data_aggregation.py
@@ -13,65 +13,145 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ========================================================================
+import json
 import re
 import requests
 from bs4 import BeautifulSoup
 
 __author__ = 'Jinho D. Choi'
 
-TIME = re.compile('(\d{1,2}:\d\d)\s+([A-Za-z]+)')
-
-def get_key(time, day):
-    return (time, day)
+TIME = re.compile('(\d{1,2}):(\d\d)\s*([AaPp]\.?\s*[Mm]\.?)?')
 
 
-def read_exam_schedule(url):
+def norm_time(hour, minute, period):
+    h = int(hour)
+    m = int(minute)
+
+    if period:
+        p = period[0].upper()
+        if p == 'P': h += 12
+
+    return h * 100 + m
+
+
+def norm_days(days):
+    DAYS = [('M', 0), ('TU', 1), ('W', 2), ('TH', 3), ('F', 4)]
+    days = days.upper()
+    b = ['0'] * 5
+
+    for d, i in DAYS:
+        if d in days:
+            b[i] = '1'
+            days = days.replace(d, '')
+
+    if 'T' in days:
+        b[1] = '1'
+        days = days.replace('T', '')
+
+    return int(''.join(b), 2)
+
+
+def course_title(section):
+    a = section.find('a')
+    t = a.text.split()
+    return t[0], ' '.join(t[1:])
+
+
+def course_days(section):
+    days = [day.string.strip() for day in section.find_all('span', {'class': 'day'})]
+    return ''.join(days) if days else '-'
+
+
+def course_time(section):
+    time = section.find('div', {'class': 'time'}).string
+    if time:
+        t = TIME.findall(time)
+        return norm_time(*t[0]), norm_time(*t[1])
+    else:
+        return -1, -1
+
+
+def course_location(section):
+    loc = section.find('div', {'class': 'location'}).string
+    return loc if loc else '-'
+
+
+def course_instructors(section):
+    its = [inst.string.strip() for inst in section.find_all('div', {'class': 'instructor'}) if inst.string]
+    return '; '.join(its).strip() if its else '-'
+
+
+def extract_exam_schedule(url):
     r = requests.get(url)
-    top = BeautifulSoup(r.text, 'html.parser')
-    tbody = top.find('tbody')
+    html = BeautifulSoup(r.text, 'html.parser')
+    tbody = html.find('tbody')
     schedule = {}
 
     for tr in tbody.find_all('tr'):
         tds = tr.find_all('td')
-        if len(tds) != 4: continue
+        t = tds[0].string.split()
 
-        m = TIME.match(tds[0].string.strip())
-        if m:
-            key = get_key(m.group(1), m.group(2))
-            day = tds[1].string.strip()
-            date = tds[2].string.strip()
-            time = tds[3].string.strip()
+        if len(t) == 2:
+            time = None
+            m = TIME.match(t[0])
+            if m: time = norm_time(m.group(1), m.group(2), m.group(3))
+            days = norm_days(t[1])
 
-            schedule[key] = (day, date, time)
+            if time and days:
+                if time < 700: time += 1200
+                key  = (time, days)
+                day  = tds[1].string.strip()
+                date = tds[2].string.strip()
+                time = tds[3].string.strip()
+                schedule[key] = (day, date, time)
 
     return schedule
 
 
+def extract_course_info(term_id):
+    url = 'http://atlas.college.emory.edu/schedules/index.php'
+    s = requests.session()
+    s.get(url, params={'t': term_id})
+    programs = ['QTM']
+    course_info = {}
+
+    for program in programs:
+        r = s.get(url, params={'select': program})
+        html = BeautifulSoup(r.text, 'html.parser')
+        for course in html.find_all('div', {'class': 'course'}):
+            course_number = course.find('h3').string
+            for section in course.find_all('li', {'class': 'section'}):
+                section_id, title = course_title(section)
+                days = norm_days(course_days(section))
+                time = course_time(section)
+                location = course_location(section)
+                instructors = course_instructors(section)
+                course_info[(course_number, section_id)] = (title, days, time, location, instructors)
+
+    return course_info
 
 
-url = 'http://registrar.emory.edu/faculty-staff/exam-exam_schedule/spring-2018.html'
-r = requests.get(url)
-print(r)
+if __name__ == '__main__':
+    url = 'http://registrar.emory.edu/faculty-staff/exam-schedule/spring-2018.html'
+    exam_schedule = extract_exam_schedule(url)
+    course_info = extract_course_info('5181')
+    course_exam = {}
 
-html = BeautifulSoup(r.text, 'html.parser')
-tbody = html.find('tbody')
-exam_schedule = {}
+    for k, v in course_info.items():
+        days = v[1]
+        time = v[2]
+        key = (time[0], days)
 
-for tr in tbody.find_all('tr'):
-    tds  = tr.find_all('td')
-    meet = tds[0].string
-    day  = tds[1].string
-    date = tds[2].string
-    time = tds[3].string
-    exam_schedule[meet] = (day, date, time)
+        if key in exam_schedule:
+            course_exam['-'.join(k)] = exam_schedule[key]
 
-for k, v in exam_schedule.items():
-    print('%10s: %s' % (k, str(v)))
+    jsonfile = '../dat/courses/course_exam_spring_2018.json'
 
-m = TIME.match('8:00 MW')
-print('Time: %s, Day: %s' % (m.group(1), m.group(2)))
-m = TIME.match('12:30 TThF')
-print('Time: %s, Day: %s' % (m.group(1), m.group(2)))
+    with open(jsonfile, 'w') as fout:
+        json.dump(course_exam, fout)
 
-m = TIME.match('Math*')
-print(m)
+    with open(jsonfile) as fin:
+        d = json.load(fin)
+
+    for k, v in d.items():
+        print(k, v)
